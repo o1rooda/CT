46ce1268f999f7601456d3a94cdd4b3cb8be524b
Rooda <o1roodaa@bitbucket.org>
2015-04-07 15:21:10 +0900

    reg;\ xxx

---
+++ Logisticregression/logRegres.py
@@ -0,0 +1,9 @@
+def loadDataSet():
+    dataMat = []; labelMat = []
+    fr = open('testSet.txt')
+    for line in fr.readlines():
+        lineArr = line.strip().split()
+        dataMat.append([1.0, float(lineArr[0]), float(lineArr[1])])
+        labelMat.append(int(lineArr[2]))
+    return dataMat,labelMat
+
