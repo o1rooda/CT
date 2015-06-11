1b3253d5065f78163614aca025e69f4b2553a163
Rooda <o1roodaa@bitbucket.org>
2015-06-11 15:01:10 +0900

    20150611

---
+++ knn/knn_rd.py
@@ -0,0 +1,21 @@
+#20150610_rd
+
+import os
+import numpy as np
+
+def createDataSet():
+    group = array([[1.0,1.1],[1.0,1.0],[0,0],[0,0.1]])
+    labels = ['A','A','B','B']
+    return group, labels
+
+def classify0(inX, dataSet, labels, k):
+    dataSetsize = dataSet.shape[0]
+    diffMat = tile(inX, (dataSetSize, 1)) - dataSet
+    sqDiffMat = diffMat**2
+    sqDistances = sqDistances**0.5
+    classCount = {}
+    for i in range(k):
+	voteIlabel = labels[sortedDistIndicies[i]]
+	classCount[voteIlabel] = classCount.get(voteIlabel, 0) + 1
+    sortedClassCount = sorted(classCount.iteritems(), key = operator.itemgetter(1), reverse = True)
+    return sortedClassCount[0][0]
